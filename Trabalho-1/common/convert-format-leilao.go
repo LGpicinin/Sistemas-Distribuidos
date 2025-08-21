bf7f4446c2979fbc956f8c2289e0c9a86cdac097
Doglas Franco Maurer da Rocha <doglas_rocha@protonmail.ch>
2025-08-20 22:45:34 -0300

    marshal

--- Trabalho-1/common/convert-format-leilao.go
+++ Trabalho-1/common/convert-format-leilao.go
@@ -1,25 +1,22 @@
 package common
 
-func LeilaoToString(leilao Leilao) []string {
-	var leilaoString []string
+import "encoding/json"
 
-	leilaoString = append(leilaoString, leilao.ID)
-	leilaoString = append(leilaoString, leilao.description)
-	leilaoString = append(leilaoString, leilao.startDate.String())
-	leilaoString = append(leilaoString, leilao.endDate.String())
-
-	return leilaoString
-}
-
-func LeiloesToCsv(leiloes []Leilao) [][]string {
-	var data [][]string
-	var leilaoString []string
-
-	for i, leilao := range leiloes {
-		leilaoString = LeilaoToString(leilao)
-		data = append(data, leilaoString)
-		print(i)
+func LeilaoToByteArray(leilao Leilao) []byte {
+	leilaoByteArray, err := json.Marshal(leilao)
+	if err != nil {
+		FailOnError(err, "Erro ao converter leilao para []byte")
 	}
 
-	return data
+	return leilaoByteArray
+}
+
+func ByteArrayToLeilao(byteArray []byte) Leilao {
+	var leilao Leilao
+	err := json.Unmarshal(byteArray, &leilao)
+	if err != nil {
+		FailOnError(err, "Erro ao converter []byte para leilao")
+	}
+
+	return leilao
 }
