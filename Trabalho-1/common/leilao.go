521420840d4b612ab8d4f7db506deed7f881394d
Doglas Franco Maurer da Rocha <doglas_rocha@protonmail.ch>
2025-08-20 22:45:51 -0300

    sorts

--- Trabalho-1/common/leilao.go
+++ Trabalho-1/common/leilao.go
@@ -3,19 +3,31 @@ package common
 import "time"
 
 type Leilao struct {
-	ID          string
-	description string
-	startDate   time.Time
-	endDate     time.Time
+	ID          string    `json:"id"`
+	Description string    `json:"description"`
+	StartDate   time.Time `json:"start_date"`
+	EndDate     time.Time `json:"end_date"`
 }
 
 func CreateLeilao(ID string, description string, startDate time.Time, endDate time.Time) Leilao {
 	var leilao Leilao = Leilao{
 		ID:          ID,
-		description: description,
-		startDate:   startDate,
-		endDate:     endDate,
+		Description: description,
+		StartDate:   startDate,
+		EndDate:     endDate,
 	}
 
 	return leilao
 }
+
+type ByStartDate []Leilao
+
+func (a ByStartDate) Len() int           { return len(a) }
+func (a ByStartDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByStartDate) Less(i, j int) bool { return a[i].StartDate.Compare(a[j].StartDate) == -1 }
+
+type ByEndDate []Leilao
+
+func (a ByEndDate) Len() int           { return len(a) }
+func (a ByEndDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByEndDate) Less(i, j int) bool { return a[i].EndDate.Compare(a[j].EndDate) == -1 }
