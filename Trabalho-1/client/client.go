a5aa77dcbc587b5aa396c54a4429f19331a5484d
Doglas Franco Maurer da Rocha <doglas_rocha@protonmail.ch>
2025-08-23 22:31:18 -0300

    definicao id cliente

--- Trabalho-1/client/client.go
+++ Trabalho-1/client/client.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	common "common"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/rabbitmq/amqp091-go"
 )
 
 func consomeLeilaoIniciado(msgs <-chan amqp091.Delivery) {
 	for d := range msgs {
-		log.Printf(" [x] %s", d.Body)
+		log.Printf("[MS-LEILAO] NOVO LEILÃO: %s", spew.Sdump(common.ByteArrayToLeilao(d.Body)))
 		d.Ack(true)
 	}
 }
@@ -29,6 +31,12 @@ func menu() {
 }
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Uso correto: ./client id_do_cliente")
+	}
+
+	// userId := os.Args[1]
+
 	conn, ch := common.ConnectToBroker()
 	defer conn.Close()
 	defer ch.Close()
