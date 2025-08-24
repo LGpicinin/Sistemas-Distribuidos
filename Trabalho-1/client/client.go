a99bb392a17ab886973faa1d8341324312c14f07
Doglas Franco Maurer da Rocha <doglas_rocha@protonmail.ch>
2025-08-24 13:43:23 -0300

    logica de leitura/criacao de chaves

--- Trabalho-1/client/client.go
+++ Trabalho-1/client/client.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	common "common"
 
@@ -23,7 +28,7 @@ func hello() {
 	fmt.Println("Caso deseje registrar um lance, pressione Enter. Caso deseje sair, aperte CTRL+C")
 }
 
-func publishLance(q amqp091.Queue, ch *amqp091.Channel, leilaoId string, userId string) {
+func publishLance(q amqp091.Queue, ch *amqp091.Channel, leilaoId string, userId string, publicKey *rsa.PublicKey, privateKey *rsa.PrivateKey) {
 	var value float32
 	fmt.Print("Qual o valor do lance que deseja fazer? ")
 	fmt.Scanf("%f", &value)
@@ -32,23 +37,76 @@ func publishLance(q amqp091.Queue, ch *amqp091.Channel, leilaoId string, userId 
 
 }
 
-func menu(userId string, q amqp091.Queue, ch *amqp091.Channel) {
+func menu(userId string, q amqp091.Queue, ch *amqp091.Channel, publicKey *rsa.PublicKey, privateKey *rsa.PrivateKey) {
 	var input string
 	for fmt.Scanf("%s", &input); ; fmt.Scanf("%s", &input) {
 		var leilaoId string
-		fmt.Print("Digite o ID do Leilão em que deseja registrar um lance, e o valor do lance: ")
+		fmt.Print("Digite o ID do Leilão em que deseja registrar um lance: ")
 		fmt.Scanf("%s", &leilaoId)
 
+		publishLance(q, ch, leilaoId, userId, publicKey, privateKey)
 	}
+}
+
+func createKeys(userId string) (*rsa.PublicKey, *rsa.PrivateKey) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalf("Error generating key pair: %v", err)
+	}
+	publicKey := &privateKey.PublicKey
+
+	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	})
+	err = os.WriteFile(fmt.Sprintf("./client/keys/private/%s.pem", userId), privateKeyPEM, 0600)
+	if err != nil {
+		log.Fatalf("Error saving private key: %v", err)
+	}
+
+	publicKeyBytes := x509.MarshalPKCS1PublicKey(publicKey)
+	publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: publicKeyBytes,
+	})
+
+	err = os.WriteFile(fmt.Sprintf("./client/keys/public/%s.pem", userId), publicKeyPEM, 0644)
+	if err != nil {
+		log.Fatalf("Error saving public key: %v", err)
+	}
+
+	err = os.WriteFile(fmt.Sprintf("./ms-lance/keys/public/%s.pem", userId), publicKeyPEM, 0644)
+	if err != nil {
+		log.Fatalf("Error saving public key: %v", err)
+	}
+
+	return publicKey, privateKey
+}
+
+func initClient(args []string) (*rsa.PublicKey, *rsa.PrivateKey) {
+	userId := os.Args[1]
+
+	entries, _ := os.ReadDir("./client/keys/public")
+	for _, entry := range entries {
+		if strings.Contains(entry.Name(), userId) {
+			publicKey, _ := common.ReadAndParseKey(fmt.Sprintf("%s/%s", "./client/keys/public", entry.Name()))
+			_, privateKey := common.ReadAndParseKey(fmt.Sprintf("%s/%s", "./client/keys/private", entry.Name()))
+
+			return publicKey, privateKey
+		}
+
+	}
+
+	return createKeys(userId)
 }
 
 func main() {
 	if len(os.Args) != 2 {
-		log.Fatal("Uso correto: ./client id_do_cliente")
+		log.Fatal("Uso correto: ./bin/client id_do_cliente")
 	}
 
-	userId := os.Args[1]
-
+	publicKey, privateKey := initClient(os.Args)
 	conn, ch := common.ConnectToBroker()
 	defer conn.Close()
 	defer ch.Close()
