611488220f56a51892dccf1fd24bf5c7e9a98c0b
LGpicinin <lgpicinin@gmail.com>
2025-11-12 10:50:39 -0300

    microsservices

--- Trabalho-4/microservices/common/constants.go
+++ Trabalho-4/microservices/common/constants.go
@@ -7,4 +7,6 @@ const (
 	QUEUE_LEILAO_VENCEDOR   string = "leilao_vencedor"
 	QUEUE_LEILAO_INICIADO   string = "leilao_iniciado"
 	QUEUE_LEILAO_FINALIZADO string = "leilao_finalizado"
+	QUEUE_LINK_PAGAMENTO    string = "link_pagamento"
+	QUEUE_STATUS_PAGAMENTO  string = "status_pagamento"
 )
---
+++ Trabalho-4/microservices/ms-pagamento/go.mod
@@ -0,0 +1,9 @@
+module ms-pagamento
+
+go 1.25.0
+
+require github.com/rabbitmq/amqp091-go v1.10.0
+
+require common v0.0.0-00010101000000-000000000000 // indirect
+
+replace common => ../common
